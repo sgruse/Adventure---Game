ef35572f81c63da2a9f3faaa04289e7be377f4c2
Sam Gruse <sgruse89@gmail.com>
2016-04-07 13:04:31 -0700

    Implementing canvas for a game board

---
+++ practice-pong-game/js/js.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = '#FF0000';
+ctx.fill();
+ctx.closePath();
